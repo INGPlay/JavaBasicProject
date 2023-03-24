f482572d464ef0501c2497158b53d5ef779d7203
김병주 <atker14@gmail.com>
2023-03-24 19:26:49 +0900

    구조 잡기

---
+++ JavaBasicProject/src/Main.java
@@ -0,0 +1,11 @@
+import notebook.menu.MainMenu;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+
+        mainMenu.menu();
+    }
+}
